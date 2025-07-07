53e224413a77550d72c1e61954a269016a44949b
Swaliha <swalihaswali100@gmail.com>
2025-07-07 10:31:53 +0530

    Add files via upload

---
+++ EXP 1/3.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Sum :",a+b)
+print("Sub:",a-b)
+print("mul:",a*b)
+print("Div:",a/b)
+print("Modulo:",a%b)
+print("Exponentiation:",a**b)
+print("floor division:",a//b)
+
---
+++ EXP 1/6.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the number:"))
+if a>0:
+	print("positive number")
+elif a==0:
+	print("number is zero")
+else :
+	print("Number is negative")
